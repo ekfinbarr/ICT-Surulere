6a3c24e84b9d352ef5f45996f436a42ca45b154f
ekFinbarr <ekfinbarr@gmail.com>
2020-03-05 10:42:06 +0100

    Initial commit for client

--- Server/routes/api.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-Route::post('/register', 'Auth\AuthController@register');
-Route::post('/login', 'Auth\AuthController@login');
-Route::post('/logout', 'Auth\AuthController@logout');
-
-
-Route::get('/user', 'Auth\AuthController@user');
